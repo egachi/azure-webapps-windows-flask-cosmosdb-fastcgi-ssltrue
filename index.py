142b1629d1b0dae841218aa1ae7d3eb40cd0ec5b
mredison <egachi@live.com>
2018-05-07 14:21:19 -0500

    Initial Commit

---
+++ index.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template,request,redirect,url_for # For flask implementation
+from bson import ObjectId # For ObjectId to work
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+title = "TODO with Flask"
+heading = "ToDo Reminder"
+
+##Un-Comment when running against the Cosmos DB Emulator
+client = MongoClient("mongodb://edisga:C9GMVNx4bBrUNMeI0L6A1PQVz2RKkkeq9cra7w3WQBKEYouvgHOwF6goGvkHGNpHT7MgwwN2FQzikdxBnhN9AA==@edisga.documents.azure.com:10255/?ssl=true&replicaSet=globaldb") #host uri
+db = client.admin    #Select the database
+users = db.users #Select the collection
+
+def redirect_url():
+    return request.args.get('next') or \
+           request.referrer or \
+           url_for('index')
+
+@app.route("/list")
+def lists ():
+	#Display the all Tasks
+	users_l = users.find()
+	a1="active"
+	return render_template('index.html',a1=a1,users=users_l,t=title,h=heading)
+
+@app.route("/")
+@app.route("/uncompleted")
+def tasks ():
+	#Display the Uncompleted Tasks
+	users_l = users.find({"done":"no"})
+	a2="active"
+	return render_template('index.html',a2=a2,users=users_l,t=title,h=heading)
+
+
+@app.route("/completed")
+def completed ():
+	#Display the Completed Tasks
+	users_l = users.find({"done":"yes"})
+	a3="active"
+	return render_template('index.html',a3=a3,users=users_l,t=title,h=heading)
+
+@app.route("/done")
+def done ():
+	#Done-or-not ICON
+	id=request.values.get("_id")
+	task=users.find({"_id":ObjectId(id)})
+	if(task[0]["done"]=="yes"):
+		users.update({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
+	else:
+		users.update({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
+	redir=redirect_url()	# Re-directed URL i.e. PREVIOUS URL from where it came into this one
+
+#	if(str(redir)=="http://localhost:5000/search"):
+#		redir+="?key="+id+"&refer="+refer
+
+	return redirect(redir)
+
+#@app.route("/add")
+#def add():
+#	return render_template('add.html',h=heading,t=title)
+
+@app.route("/action", methods=['POST'])
+def action ():
+	#Adding a Task
+	name=request.values.get("name")
+	desc=request.values.get("desc")
+	date=request.values.get("date")
+	pr=request.values.get("pr")
+	users.insert({ "name":name, "desc":desc, "date":date, "pr":pr, "done":"no"})
+	return redirect("/list")
+
+@app.route("/remove")
+def remove ():
+	#Deleting a Task with various references
+	key=request.values.get("_id")
+	users.remove({"_id":ObjectId(key)})
+	return redirect("/")
+
+@app.route("/update")
+def update ():
+	id=request.values.get("_id")
+	task=users.find({"_id":ObjectId(id)})
+	return render_template('update.html',tasks=task,h=heading,t=title)
+
+@app.route("/action3", methods=['POST'])
+def action3 ():
+	#Updating a Task with various references
+	name=request.values.get("name")
+	desc=request.values.get("desc")
+	date=request.values.get("date")
+	pr=request.values.get("pr")
+	id=request.values.get("_id")
+	users.update({"_id":ObjectId(id)}, {'$set':{ "name":name, "desc":desc, "date":date, "pr":pr }})
+	return redirect("/")
+
+@app.route("/search", methods=['GET'])
+def search():
+	#Searching a Task with various references
+
+	key=request.values.get("key")
+	refer=request.values.get("refer")
+	if(key=="_id"):
+		todos_l = users.find({refer:ObjectId(key)})
+	else:
+		todos_l = users.find({refer:key})
+	return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
+
+@app.route("/about")
+def about():
+	return render_template('credits.html',t=title,h=heading)
+
+# define for IIS module registration.
+
+wsgi_app = app.wsgi_app
+
+if __name__ == "__main__":
+
+    app.run()
+
+
+
